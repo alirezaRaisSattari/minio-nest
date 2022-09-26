35f2bf6d90ca62aa3f3528beb6c26a5c48ee0508
Alireza Sattari <alirezaraissattari@gmail.com>
2022-09-26 10:26:32 +0330

    first commit

---
+++ src/minio-client/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  MINIO_ENDPOINT: '127.0.0.1',
+  MINIO_PORT: 9000,
+  MINIO_ACCESSKEY: '3cpDvd40UsNfiw7K',
+  MINIO_SECRETKEY: 'VfHjXgmGDJXoPE28emzZsgKDSGDxmICO',
+  MINIO_BUCKET: 'europetrip'
+}
